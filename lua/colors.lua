7d882840ca2acd8c5003ce916da5ae9e1fa20e6f
fadiinho <ffadiinho@gmail.com>
2022-09-03 02:06:29 -0300

    refactor: make colors be in one single file

---
+++ lua/colors.lua
@@ -0,0 +1,26 @@
+local light_100 = "#E0DDE5"
+local dark_100 = "#221B32"
+
+return {
+	cmyk_black = "#130E0F",
+	bg = dark_100,
+	dark_200 = "#2B223F",
+	dark_300 = "#382C51",
+	dark_400 = "#302546",
+	dark_500 = "#463767",
+	purrrple = "#7515EF",
+	lavendrrr = "#B596F8",
+	cerrrulean = "#15AEEF",
+	electric_crrrimson = "#EF1563",
+	tangerrrine = "#EF9815",
+	highlighterrr_yellow = "#EBEF15",
+	grrreen = "#15EFAE",
+	grrreen_1 = "#00CC8F",
+	hot_pink = "#EF15BF",
+	fg = light_100,
+	light_200 = "#D5D2DC",
+	light_300 = "#BCB9C2",
+	light_400 = "#807694",
+	light_500 = "#878292",
+	bright_white = "#FFFFFF",
+}
